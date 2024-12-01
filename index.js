22139abdb249fbabb02388592c20eff980a10b3e
Rohit Singh <rohitsinghukboss@gmail.com>
2024-12-01 22:46:43 +0530

    code hai ya nhi

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const age = 24;
-const name = "rohit singh ";
+const name1 = "rohit singh ";
 const number = "56";
 
 function get (){
-    console.log('my name is ' + name)
+    console.log('my name is ' + name1)
 
+}function gets (){
+    console.log("mohit singh")
 }
